a4df837104eaeff47d493b11e92f37980723cab3
Claudia Nijholt <nijholt.claudia.2107@gmail.com>
2024-11-22 11:52:51 +0100

    Add Huis and Persoon classes with appropriate methods and relationships

---
+++ src/Huis.java
@@ -0,0 +1,29 @@
+public class Huis {
+    // Attributen
+    private String adres;
+    private int bouwjaar;
+    private Persoon huisbaas;
+
+    // Constructor
+    public Huis(String adres, int bouwjaar) {
+        this.adres = adres;
+        this.bouwjaar = bouwjaar;
+    }
+
+    // Setter voor huisbaas
+    public void setHuisbaas(Persoon huisbaas) {
+        this.huisbaas = huisbaas;
+    }
+
+    // Getter voor huisbaas
+    public Persoon getHuisbaas() {
+        return huisbaas;
+    }
+
+    // toString methode
+    @Override
+    public String toString() {
+        return "Huis " + adres + " is gebouwd in " + bouwjaar + "\n" +
+                "en heeft huisbaas " + huisbaas;
+    }
+}
---
+++ src/Persoon.java
@@ -0,0 +1,26 @@
+public class Persoon {
+    // Attributen
+    private String naam;
+    private int leeftijd;
+
+    // Constructor
+    public Persoon(String naam, int leeftijd) {
+        this.naam = naam;
+        this.leeftijd = leeftijd;
+    }
+
+    // Getters
+    public String getNaam() {
+        return naam;
+    }
+
+    public int getLeeftijd() {
+        return leeftijd;
+    }
+
+    // toString methode
+    @Override
+    public String toString() {
+        return naam + "; leeftijd " + leeftijd + " jaar";
+    }
+}
