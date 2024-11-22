ecc9b1c11a96c7c70589156d4ae0577d381abe57
Claudia Nijholt <nijholt.claudia.2107@gmail.com>
2024-11-22 09:50:26 +0100

    Add Som.java to calculate the sum of numbers from 0 to 39

---
+++ src/Som.java
@@ -0,0 +1,9 @@
+public class Som {
+    public static void main(String[] args) {
+        int som = 0;
+        for (int i = 0; i <= 39; i++) {
+            som += i;
+        }
+        System.out.println("De som van de getallen 0 tot en met 39 is: " + som);
+    }
+}
