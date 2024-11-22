bf9059fb9994e6023d7cd96e60ad731a8eaa1afb
Claudia Nijholt <nijholt.claudia.2107@gmail.com>
2024-11-22 11:44:33 +0100

    Add Cirkel class with radius validation and exception handling.

---
+++ src/Cirkel.java
@@ -0,0 +1,23 @@
+public class Cirkel {
+    // Attributen
+    private double radius;
+    private double x;
+    private double y;
+
+    // Constructor
+    public Cirkel(double radius, double x, double y) {
+        if (radius <= 0) {
+            throw new IllegalArgumentException("Radius moet groter dan 0 zijn!");
+        }
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+    }
+
+    // toString methode
+    @Override
+    public String toString() {
+        return "cirkel (" + x + ", " + y + ") met radius: " + radius;
+    }
+}
+
---
+++ test/CirkelTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+public class CirkelTest {
+    @Test
+    public void test() {
+        Cirkel c1 = null, c2 = null;
+
+        // Uitkomst;
+        // Zonder try catch stop de applicatie tijdens het uitvoeren
+
+        // Verklaring:
+        // De code compileert nog steeds omdat de IllegalArgumentException
+        // een runtime exception is, en de compiler vereist niet dat je die moet afvangen.
+        // De code zal compileren, maar de applicatie zal stoppen tijdens uitvoering wanneer
+        // de uitzondering optreedt.
+
+        try {
+            c1 = new Cirkel(10, 0, 0);
+            c2 = new Cirkel(0, 10, 10);
+        } catch (IllegalArgumentException iae) {
+            System.out.println(iae.getMessage());
+        }
+
+        System.out.println(c1);
+        System.out.println(c2);
+    }
+}
