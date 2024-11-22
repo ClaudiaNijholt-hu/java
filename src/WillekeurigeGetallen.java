b2cef4caf68e5ae024a4ca2a9e23c34aaf9c447b
Claudia Nijholt <nijholt.claudia.2107@gmail.com>
2024-11-22 09:53:09 +0100

    Add WillekeurigeGetallen.java to generate and print 10 random numbers.

---
+++ src/WillekeurigeGetallen.java
@@ -0,0 +1,8 @@
+public class WillekeurigeGetallen {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            double willekeurigGetal = Math.random();
+            System.out.println(willekeurigGetal);
+        }
+    }
+}
